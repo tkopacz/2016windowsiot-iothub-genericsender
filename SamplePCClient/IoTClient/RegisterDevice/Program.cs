177ff4c2fd695ade5b034a790eaf2cce62476440
tkopacz <tkopacz@tomaszkopacz.com>
2016-02-23 22:17:21 +0100

    OK, Register

---
+++ SamplePCClient/IoTClient/RegisterDevice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common;
+
+namespace RegisterDevice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Usage: \r\n",
+                "RegisterDevice <iotHubOwner> <deviceName>" );
+            string connection = args[0];
+            string deviceName = args[1];
+            RegistryManager rm = RegistryManager.CreateFromConnectionString(connection);
+            Device d = new Device(deviceName);
+            rm.AddDeviceAsync(d).Wait();
+            d = rm.GetDeviceAsync(deviceName).Result;
+            Console.WriteLine(d.Id);
+            Console.WriteLine(d.Authentication.SymmetricKey.PrimaryKey);
+
+        }
+    }
+}
